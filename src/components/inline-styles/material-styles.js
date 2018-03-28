233c0ae6c80dd571cc62e7a6fec9de6bfd3391bf
Jarno <jarno.raisanen@hotmail.com>
2018-03-28 21:43:02 +0300

    Tab improved

--- src/components/inline-styles/material-styles.js
+++ src/components/inline-styles/material-styles.js
@@ -9,6 +9,18 @@ const styles = {
     button: {
         position: 'absolute',
         right: '5%'
+    },
+    beerGallery: {
+        width: '80%',
+        backgroundColor: '#FFFDE7',
+        borderColor: 'black',
+        opacity: '0.9',
+        borderStyle: 'inset',
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        paddingTop: '2vh',
+        paddingBottom: '5vh'
     }
 }
 
--- src/components/url-components/Beers.jsx
+++ src/components/url-components/Beers.jsx
@@ -1,43 +1,24 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import Header from '../filler-components/Header';
+import {Table, Row, Cell} from 'react-responsive-table';
 import ipa from '../../images/container-imgs/ipa.jpg';
 import paleale from '../../images/container-imgs/paleale.jpg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import pils from '../../images/container-imgs/pils.jpg';
 import stout from '../../images/container-imgs/stout.jpg';
+import styles from '../inline-styles/material-styles'
 
 const Beers = (props) => (
   <div>
-    <Header 
-      title="Oluet"
-      handleToggle={props.handleToggle}/>
-    <div className="beer-gallery">
-      <tr className>
-        <th>Hagert IPA (7,2 %)</th>
-        <th>Beard Pale Ale (4,7 %)</th>
-      </tr>
-      <tr>
-        <th>
-          <Link to='/oluet/ipa' className="nav-link"></Link>
-          <img src={ipa}/>
-        </th>
-        <td>
-          <img src={paleale}/>
-        </td>
-      </tr>
-      <tr>
-        <th>Moustache Pils (5,8 %)</th>
-        <th>Macho Stout (9,0 %)</th>
-      </tr>
-      <tr>
-        <th>
-          <img src={pils}/>
-        </th>
-        <td>
-          <img src={stout}/>
-        </td>
-      </tr>
-    </div>
+    <Header title="Oluet" handleToggle={props.handleToggle}/>
+      <Table className="beer-gallery" style={styles.beerGallery}>
+        <Row material header key="row1">
+          <Cell minWidthPx={250} key="cell1"><h4>Hagert IPA (7,2 %)</h4><img src={ipa}/></Cell>
+          <Cell minWidthPx={250} key="cell2"><h4>Beard Pale Ale (4,7 %)</h4><img src={paleale}/></Cell>
+          <Cell minWidthPx={250} key="cell3"><h4>Moustache Pils (5,8 %)</h4><img src={pils}/></Cell>
+          <Cell minWidthPx={250} key="cell4"><h4>Macho Stout (9,0 %)</h4><img src={stout}/></Cell>
+        </Row>
+      </Table>
   </div>
 );
 
